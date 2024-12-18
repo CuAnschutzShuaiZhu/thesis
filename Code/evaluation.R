f0d4278ab7c734c50d6be34557c71e77147f364d
CuAnschutzShuaiZhu <shuai.zhu@cuanschutz.edu>
2024-12-17 22:02:55 -0700

    simulation data

--- Code/evaluation.R
+++ Code/evaluation.R
@@ -9,11 +9,13 @@ hist(model_list100[[1]]$data$plasma)
 plot(model_list100[[1]]$data)
 
 data <- model_list100[[1]]$data%>%as_tibble()
-threshold1 <- 0.08
-threshold2 <- 0.09
-true_class <-  c(rep(1,70), rep(2,30))
-pred_class <-  cut(data$plasma,breaks = c(-Inf, threshold1, threshold2, Inf),
-                   labels = c(2, 0, 1))
+dtroc <- function(data,threshold1, threshold2){
+  true_class <-  c(rep(1, nrow(data)*0.7), rep(2,nrow(data)*0.3))
+  pred_class <-  cut(data$plasma,breaks = c(-Inf, threshold1, threshold2, Inf),
+                     labels = c(2, 0, 1))
+  
+}
+
 
 roc_obj <- roc(as.numeric(true_class == 1), as.numeric(pred_class == 1))
 roc_obj <- roc(true_class, data$plasma)
