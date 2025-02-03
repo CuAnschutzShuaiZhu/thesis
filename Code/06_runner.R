5bb378ab12a0b2176f45e4059ec269dbbc8c1ec1
Zhu <shuai.zhu@cuanschutz.edu>
2025-02-03 13:09:52 -0700

    update

--- Code/06_runner.R
+++ Code/06_runner.R
@@ -6,7 +6,7 @@ source('Code/04_data analysis.R')
 
 ## example of simulation
 start <- Sys.time()
-cat('start runing', as.character(start))
+cat('start runing', as.character(start),'\n')
 parallel_sim(200, 1000, 0.3)
 parallel_sim(200, 1000, 0.5)
 parallel_sim(200, 1000, 0.8)
@@ -16,9 +16,9 @@ difftime(end, start, units = 'mins')%>%print()
 
 ## simulation version 2
 #samplesize200partition0.1traintest <- do.call(rbind, lapply(readRDS("DataProcessed/samplesize200partition0.1traintest.RDS"), get_evaluation_metric2))
-samplesize200partition0.3traintest <- do.call(rbind, lapply(readRDS("DataProcessed/train test/samplesize200partition0.3traintest.RDS"), get_evaluation_metric2))
-samplesize200partition0.5traintest <- do.call(rbind, lapply(readRDS("DataProcessed/train test/samplesize200partition0.5traintest.RDS"), get_evaluation_metric2))
-samplesize200partition0.8traintest <- do.call(rbind, lapply(readRDS("DataProcessed/train test/samplesize200partition0.8traintest.RDS"), get_evaluation_metric2))
+samplesize200partition0.3traintest <- do.call(rbind, lapply(readRDS("DataProcessed/samplesize200partition0.3traintest.RDS"), get_evaluation_metric2))
+samplesize200partition0.5traintest <- do.call(rbind, lapply(readRDS("DataProcessed/samplesize200partition0.5traintest.RDS"), get_evaluation_metric2))
+samplesize200partition0.8traintest <- do.call(rbind, lapply(readRDS("DataProcessed/samplesize200partition0.8traintest.RDS"), get_evaluation_metric2))
 cbind(train_size = c('0.3', '0.5', '0.8'), 
       rbind(samplesize200partition0.3traintest%>%col_summary(),
             samplesize200partition0.5traintest%>%col_summary(),
